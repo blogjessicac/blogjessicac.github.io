b3a9b45ebbc6e317eb01f2fc061b93c28b6aa4c3
rcplne <richlandnepieclub@gmail.com>
2020-03-14 15:38:01 -0400

    updated files

--- lydea/lydeas3.0/lydeas stuff/helloworld-lydea.py
+++
@@ -1,8 +0,0 @@
-print('Hi')
-print('i am a computer programmer')
-my_name= 'lydea'
-print('my name is')
-print(my_name)
-users_name=input ('what is your name?')
-print(users_name)
-print('hello ' + users_name + ' ,welcome')
