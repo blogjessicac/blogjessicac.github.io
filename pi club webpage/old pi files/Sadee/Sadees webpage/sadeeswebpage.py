002638e9f0400258454e2a6526e0c67bc030dfe2
blogjessicac <53352049+blogjessicac@users.noreply.github.com>
2021-08-22 19:56:43 -0400

    Add files via upload

---
+++ pi club webpage/old pi files/Sadee/Sadees webpage/sadeeswebpage.py
@@ -0,0 +1,14 @@
+ <html>
+
+   <head>
+       <limk rel="stylesheet" href= "style.css">
+   </head>
+
+   <body>
+
+      <h1>project showcase</h1>
+<p>
+      <a href="https://www.richlandlibrary.com/northeast">click here for money</a>
+</p>
+    </body>
+</html>
